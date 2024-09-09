ee9c5e45d7983de72fa924deffffa2d4e4ab48e5
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-09-08 23:16:54 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    año = input("Ingrese un año:")
+
+    ye = int(año)  # Convierte el año a entero
+    if (ye % 400 == 0) or (ye % 4 == 0 and ye % 100 != 0):
+        print(f"El año {ye} es bisiesto")
+    else:
+        print(f"El año {ye} no es bisiesto")
